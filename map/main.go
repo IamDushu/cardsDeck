61ecf4a9a9833144c299e0462f3b94c31e66a3d8
IamDushu <hey.dushyanth@gmail.com>
2023-11-11 14:33:23 +0530

    Manipulating maps

--- map/main.go
+++ map/main.go
@@ -4,10 +4,23 @@ import "fmt"
 
 func main() {
 
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"white": "#ffffff",
-	}
+	//Different ways of initializing map
+
+	// var colors map[string]string
+
+	colors := make(map[string]string)
+
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"white": "#ffffff",
+	// }
+
+	colors["white"] = "#ffffff"
+	colors["black"] = "#000000"
+
+	fmt.Println(colors["black"]) //Use square braces to access a value
+
+	delete(colors, "black")
 
 	fmt.Println(colors)
 
