0dbebec247c7eb5aa13ac23f0a6df131b273abc7
IamDushu <hey.dushyanth@gmail.com>
2023-11-12 18:26:45 +0530

    Custom Writer

--- http/main.go
+++ http/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 
@@ -15,11 +17,19 @@ func main() {
 		os.Exit(1)
 	}
 
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
 
 	// // var data []byte --> Refer notes; Try doing with this
 	// data := make([]byte, 99999)
 	// dataRead, _ := resp.Body.Read(data)
 	// fmt.Println(string(data)) //----> if we just write Println(data) it just prints a slice.
 	// fmt.Println(dataRead)     //------> no. of bytes read by read func
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
